23a111a0059232772090623bad6c0cd381bcc1b4
Nabin <nkbhaie2017@gmail.com>
2021-02-16 10:01:46 -0700

    add arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,18 @@
+// GitHub Exercise 
+
+/*
+        Part I
+
+1. Create a local repository and add and commit some files
+2. Create a remote repository and push your code from the local repo to the remote
+3. Create a local branch and add and commit some files
+4. Push that local branch to GitHub to create a remote branch
+*/
+//1. I created a local repository in my machine called Local_Repo.
+//2. Remote repository called Remote_Repo is created and the following codes are pushed ot it via terminal.
+
+
+const arrowBasics = (a, b) => {
+    return a + b;
+}
+console.log(arrowBasics(3, 4));
